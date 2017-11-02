369153dba2fc249595753dc129334f5488a52587
Muresan Andrei <andrei.muresan@red-to-blue.com>
2017-11-02 16:36:25 +0200

    change db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171102140324) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "facts", force: :cascade do |t|
     t.string "fact"
     t.datetime "created_at", null: false
